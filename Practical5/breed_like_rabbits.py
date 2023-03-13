4e91bc6be412fee913c366671e54435f91a0f875
Xinyu-ZJE <xinyu.22@intl.zju.edu.cn>
2023-03-14 01:09:05 +0800

    ribbits

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,1 +1,12 @@
 #repeat
+#	count the generations of rabbits
+#	how many rabbits are there already?
+#		If the total number is smaller than 100:keep counting
+#		If larger than 100: print the generation number
+
+num=2 #the initial number is 2
+gen=1 #the initial generation is 1
+while num<100:
+ gen=gen+1
+ num=pow (2,gen)
+print (gen)
