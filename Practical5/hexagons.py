ff36f449aaa8ec7275f184168fcb48d2d434a999
Xinyu-ZJE <xinyu.22@intl.zju.edu.cn>
2023-03-13 23:33:24 +0800

    add comments

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,7 +1,14 @@
 #repeat
-for i in range (1,5):
-# i represnts ith hexagonal number
- h=i*(2*i-1)
-# h represents the hexagonal number
- print ("hexagonal number is",h)
+#calculate the hexagnoal number from the first to the fifth
+for i in range (1,6): # i represnts ith hexagonal number
+ h=i*(2*i-1) # h represents the hexagonal number
+#output the value
+ print ("hexagonal number",i,"is",h)
+
+#the output is
+#  hexagonal number 1 is 1
+#  hexagonal number 2 is 6
+#  hexagonal number 3 is 15
+#  hexagonal number 4 is 28
+#  hexagonal number 5 is 45
 
