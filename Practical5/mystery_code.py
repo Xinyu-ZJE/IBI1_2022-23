5ee6239fa1ae0b4a18887e56f2ddc9ae7439c377
Xinyu-ZJE <xinyu.22@intl.zju.edu.cn>
2023-03-14 00:10:59 +0800

    add hypothesis and comments

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:
+# Answer:Randomly generate integers between 1 and 100 for 10 times and print the biggest one.
 
 # Import libraries
 # randint allows drawing a random number,
@@ -10,12 +10,16 @@ from random import randint
 # e.g. ceil(4.2)=5
 from math import ceil
 
+#initialize the variables
 progress=0
 stored_random_number=0
+#create a loop which will run 10 times
 while progress<10:
 	progress+=1
+#generate a random integer between 1 to 100
 	n = randint(1,100)
+#use the variable stored_random_number to store the biggest n
 	if n > stored_random_number:
 		stored_random_number = n
-
+#print the biggest integer
 print(stored_random_number)
