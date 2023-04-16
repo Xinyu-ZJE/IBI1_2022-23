91be2e40262987f49bd76bacadcb96e751aa95a2
Xinyu-ZJE <xinyu.22@intl.zju.edu.cn>
2023-04-16 20:44:15 +0800

    modify the task1

--- practical9/cds_counter.py
+++ practical9/cds_counter.py
@@ -5,5 +5,6 @@ seq_1=re.findall('ATG(.+)',seq)
 seq_2=''.join(seq_1)
 number1=seq_2.count('TAA')
 number2=seq_2.count('TGA')
-Total_number=number1+number2
+number3=seq_2.count('TAG')
+Total_number=number1+number2+number3
 print(Total_number)
