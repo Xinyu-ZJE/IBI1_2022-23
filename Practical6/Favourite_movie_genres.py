6174b9e79656cbd53ed83f4700e2df3192757ed5
Xinyu-ZJE <xinyu.22@intl.zju.edu.cn>
2023-05-22 16:49:58 +0800

    delete some repeating information

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -5,36 +5,15 @@ movie={'Comedy':73,'Action':42,'Romance':38,'Fantasy':28,'Science-fictioin':22,'
 #two methods to  realize the interaction with users
 # the first one: chosen
 chosen=input('the movie genre:(capitalize the first letter)')
-a=movie.get(chosen,'notfound')
-print(a)
-# the second one: if loop
-key=input('the movie genre:(capitalize the first letter)')
-if key in movie.keys():
-    print(movie[key])
-else:
-    print('notfound')
-#draw pie chat in two methods
-#the first one, do not depent on the dictionary
-import numpy as np
+print(movie.get(chosen,'notfound'))
+
 import matplotlib.pyplot as plt
-# the label is counter-clockwise
-labels ='Comedy','Action','Romance','Fantasy','Science-fiction','Horror','Crime','Dpcumentary','History','War'
-sizes= [73,42,38,28,22,19,18,12,8,7]
+#import the dictionary in to the pie chart
+labels=movie.keys()
+sizes=movie.values()
 #the numbers of explode represent the distance of the sector from the center of the circle
 explode=(0,0,0,0,0,0,0,0,0,0.3)
 #autopct is the significant figures
 plt.pie(sizes,explode=explode,labels=labels,autopct="%1.1f%%")
-#ensure that the pie is drawn as a circle
-plt.axis('equal')
-plt.title ('Favourite movie genres among Chinese university students(Method1)')
-plt.show()
-
-#The second one is to import the dictionary into the pie chart
-import numpy as np
-import matplotlib.pyplot as plt
-labels=movie.keys()
-sizes=movie.values()
-explode=(0,0,0,0,0,0,0,0,0,0.3)
-plt.pie(sizes,explode=explode,labels=labels,autopct="%1.1f%%")
-plt.title ('Favourite movie genres among Chinese university students(Method2)')
+plt.title ('Favourite movie genres among Chinese university students')
 plt.show()
